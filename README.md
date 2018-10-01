f6f978b53e278ad375b65baa8868b237624f4287
Varun2604 <iyervarun2604@gmail.com>
2018-10-02 03:52:04 +0530

    - basic schema migrations (tested in mysql) - committing app directory

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - A facebook chatbot built over flask(http://flask.pocoo.org/), that can generate leads for your business.
 - Just plug the AI engine with the project.
+
+
+Steps for Database migrations: (all commands to be executed from the root)
+- flask db init (will create a migrations directory)
+- flask db migrate (will create the necessary migrations)
+- flask db upgrade (will conduct the necessary migrations)
---
+++ app/models.py
@@ -0,0 +1,20 @@
+from app import db
+
+class Lead(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender = db.Column(db.Integer, index=True, primary_key=True)
+    email = db.Column(db.String(120), index=True)
+    phone = db.Column(db.String(120), index=True)
+    entity = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<Lead {}>'.format(self.id)
+
+
+class Scheduled_Push_Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String(10))
+    message = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<Scheduled_Push_Message {}>'.format(self.id)
