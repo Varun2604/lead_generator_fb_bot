0c79b79b918c1769ef51a29f07f4324cb90aed29
Varun2604 <iyervarun2604@gmail.com>
2018-10-02 04:18:24 +0530

    - adding routes for subscription verification

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
+from flask import request
+from config import Config
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return "Hello, World!"
+
+@app.route('/verify_webhook_subscription', methods=["GET"])
+def verify_webhook_subscription():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == Config.SUBSCRIPTION_VERIFICATION_TOKEN:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root@localhost:3306/lead_generator'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SUBSCRIPTION_VERIFICATION_TOKEN = 'verification_token'
